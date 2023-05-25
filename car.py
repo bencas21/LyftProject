5d1a83286049ab57b2e13826a8612070fc92e5da
Benjamin Cassatt <benjamin.cassatt@gmail.com>
2023-05-25 15:40:05 -0400

    finished project

--- car.py
+++ car.py
@@ -1,12 +1,13 @@
 from Serviceable import Serviceable 
 
 class Car(Serviceable):
-    def __init__(self, engine, battery):
+    def __init__(self, engine, battery,tires):
         self.engine = engine
         self.Battery = battery 
+        self.Tires = tires 
 
     def needs_service(self):
-        if self.engine.needs_service() or self.batter.needs_service():
+        if self.engine.needs_service() or self.Battery.needs_service() or self.Tires.needs_service():
             return true
         else:
             return false
